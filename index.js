c2eb8e54ff2b3f696746501fb4d13ee61dc626f2
yajegomez <134838351+yajegomez@users.noreply.github.com>
2023-06-23 15:29:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -116,10 +116,7 @@ let interval;
 let spinCount = 0;
 let showCategoryButton = false;
 $(function() {
-  console.log(google);
-  console.log(google.maps);
-  console.log(google.maps.event);
-  google.maps.event.addEventListener("load", initialize);
+  google.maps.addEventListener("load", initialize);
   // Add an event listener to the close button or any other element that triggers the modal closing action
   const closeButton = document.getElementById("closeButton");
   closeButton.addEventListener("click", closeCategoryRestaurantsModal);
